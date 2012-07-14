008e6740d90f17feb05e3ba6daaf6dbbb06dba3b
Andrey Kolbasov <andrey.kolbasov@gmail.com>
2012-07-14 16:11:56 +0700

    Validation

--- js/gi.js
+++ js/gi.js
@@ -6,16 +6,25 @@ $(document).ready(function() {
       if (url.data('old') != url.val()) {
       	url.data('old', url.val());
       	var src = url.val();
-      	var template = '<img src="' + src + 'media/?size=';
+         var result = $('.result');
+         result.empty();
+         if(validate(src)) {
+            var template = '<img src="' + src + 'media/?size=';
    			var ending = '"/>';
-   			var result = $('.result');
    			result.append(template + 'l' + ending);
    			result.append(template + 'm' + ending);
 				result.append(template + 't' + ending);
 				$('img').on('click', function() {
 					window.open($(this).attr('src'));
-				})
-			}
-		});
-	});
+				});
+         } else {
+            result.append('<div class="alert alert-error">This is not a valid url</div>');
+         }
+		}
+	});});
 });
+
+function validate(url) {
+   var pattern = /http:\/\/instagram.com\/p\/\w*/i;
+   return pattern.test(url);
+}
